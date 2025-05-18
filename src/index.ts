d69157ada25d9d196d05200945f793fca3e217d8
Amatsuka <amatsukamao@qq.com>
2025-05-18 17:16:13 +0800

    ✨ feat(index): 在消息中添加喵后缀 - 修改 index.ts，添加 before-send 钩子以检测消息内容 - 如果消息为单行且以中文结尾，则自动在末尾添加 "喵" 以增强趣味性
    👷 ci(workflow): 添加发布工作流
    - 新增 .github/workflows/publish.yml 文件
    - 配置在主分支推送且版本变化时自动构建并发布包，避免手动操作

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import { Context, Schema } from 'koishi'
+import { console } from "inspector";
+import { Context, Schema } from "koishi";
 
-export const name = 'amatsuka-neko'
+export const name = "amatsuka-neko";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.on("before-send", (session, options) => {
+    const content = session.content;
+    if (content.split("\n").length === 1) {
+      session.content =
+        content +
+        (content[content.length - 1].match(/[\u4e00-\u9fa5]/) ? "喵" : " 喵");
+    }
+    return false;
+  });
 }
